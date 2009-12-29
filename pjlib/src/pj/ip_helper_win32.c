chakrit/pjsip

commit 7000b7b92d5e8d83cf7d2d4b8c7dfd14b132c3f6

More ticket #1000 (IPv6 interface enumeration fails due to typo error in Windows targets):  - fixed crash if GetAdaptersAddresses() returns adapter with NULL "firstUnicastAddress". This happens when the interface is disabled.
git-svn-id: http://svn.pjsip.org/repos/pjproject/trunk@3030 74dad513-b988-da41-8d7b-12977e46ad98

pjlib/src/pj/ip_helper_win32.c
@@ -237,9 +237,11 @@ static pj_status_t enum_ipv4_ipv6_interface(int af,
 	return PJ_RETURN_OS_ERROR(rc);
 
     for (i=0; i<*p_cnt && adapter; ++i, adapter = adapter->Next) {
-	SOCKET_ADDRESS *pAddr = &adapter->FirstUnicastAddress->Address;
-	ifs[i].addr.sa_family = pAddr->lpSockaddr->sa_family;
-	pj_memcpy(&ifs[i], pAddr->lpSockaddr, pAddr->iSockaddrLength);
+	if (adapter->FirstUnicastAddress) {
+	    SOCKET_ADDRESS *pAddr = &adapter->FirstUnicastAddress->Address;
+	    ifs[i].addr.sa_family = pAddr->lpSockaddr->sa_family;
+	    pj_memcpy(&ifs[i], pAddr->lpSockaddr, pAddr->iSockaddrLength);
+	}
     }
 
     *p_cnt = i;

commit 00310543d173fa735854c808b02241baceaf0941

More ticket #1000:  - fixed bug caused by r3030. If an adapter with NULL firstUnicastAddress is found, the function will return uninitialized array element.
git-svn-id: http://svn.pjsip.org/repos/pjproject/trunk@3038 74dad513-b988-da41-8d7b-12977e46ad98

pjlib/src/pj/ip_helper_win32.c
@@ -275,11 +275,16 @@ static pj_status_t enum_ipv4_ipv6_interface(int af,
     if (rc != ERROR_SUCCESS)
 	return PJ_RETURN_OS_ERROR(rc);
 
-    for (i=0; i<*p_cnt && adapter; ++i, adapter = adapter->Next) {
+    for (i=0; i<*p_cnt && adapter; adapter = adapter->Next) {
 	if (adapter->FirstUnicastAddress) {
 	    SOCKET_ADDRESS *pAddr = &adapter->FirstUnicastAddress->Address;
-	    ifs[i].addr.sa_family = pAddr->lpSockaddr->sa_family;
-	    pj_memcpy(&ifs[i], pAddr->lpSockaddr, pAddr->iSockaddrLength);
+	    if (pAddr->lpSockaddr->sa_family == PJ_AF_INET ||
+		pAddr->lpSockaddr->sa_family == PJ_AF_INET6)
+	    {
+		ifs[i].addr.sa_family = pAddr->lpSockaddr->sa_family;
+		pj_memcpy(&ifs[i], pAddr->lpSockaddr, pAddr->iSockaddrLength);
+		++i;
+	    }
 	}
     }
 