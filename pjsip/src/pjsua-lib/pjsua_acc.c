chakrit/pjsip

commit 990e0dc8d6a61edfbd0061d96183d531426cd1d8

Fix #1669: When (account/global) proxy setting is changed, unregister and reset registration internal state (acc->regc) before performing re-registration. The same mechanism is also applied for any account config changes on transport, credential info, or auth preference.
git-svn-id: http://svn.pjsip.org/repos/pjproject/trunk@4523 74dad513-b988-da41-8d7b-12977e46ad98

pjsip/src/pjsua-lib/pjsua_acc.c
@@ -924,6 +924,7 @@ PJ_DEF(pj_status_t) pjsua_acc_modify( pjsua_acc_id acc_id,
     if (acc->cfg.transport_id != cfg->transport_id) {
 	acc->cfg.transport_id = cfg->transport_id;
 	update_reg = PJ_TRUE;
+	unreg_first = PJ_TRUE;
     }
 
     /* Update keep-alive */
@@ -991,6 +992,7 @@ PJ_DEF(pj_status_t) pjsua_acc_modify( pjsua_acc_id acc_id,
     if (acc->cfg.reg_use_proxy != cfg->reg_use_proxy) {
         acc->cfg.reg_use_proxy = cfg->reg_use_proxy;
         update_reg = PJ_TRUE;
+	unreg_first = PJ_TRUE;
     }
 
     /* Global outbound proxy */
@@ -1011,6 +1013,7 @@ PJ_DEF(pj_status_t) pjsua_acc_modify( pjsua_acc_id acc_id,
 	acc->global_route_crc = global_route_crc;
 
 	update_reg = PJ_TRUE;
+	unreg_first = PJ_TRUE;
     }
 
     /* Account proxy */
@@ -1035,6 +1038,7 @@ PJ_DEF(pj_status_t) pjsua_acc_modify( pjsua_acc_id acc_id,
 	acc->local_route_crc = local_route_crc;
 
 	update_reg = PJ_TRUE;
+	unreg_first = PJ_TRUE;
     }
 
     /* Credential info */
@@ -1088,13 +1092,19 @@ PJ_DEF(pj_status_t) pjsua_acc_modify( pjsua_acc_id acc_id,
 	{
 	    acc->cred[acc->cred_cnt++] = pjsua_var.ua_cfg.cred_info[i];
 	}
+
+	update_reg = PJ_TRUE;
+	unreg_first = PJ_TRUE;
     }
 
     /* Authentication preference */
     acc->cfg.auth_pref.initial_auth = cfg->auth_pref.initial_auth;
-    if (pj_strcmp(&acc->cfg.auth_pref.algorithm, &cfg->auth_pref.algorithm))
+    if (pj_strcmp(&acc->cfg.auth_pref.algorithm, &cfg->auth_pref.algorithm)) {
 	pj_strdup_with_null(acc->pool, &acc->cfg.auth_pref.algorithm, 
 			    &cfg->auth_pref.algorithm);
+	update_reg = PJ_TRUE;
+	unreg_first = PJ_TRUE;
+    }
 
     /* Registration */
     if (acc->cfg.reg_timeout != cfg->reg_timeout) {

commit 030eceb537043e0aded6e96c5a98708f1baaca79

Re #1669: fixed bug introduced by r4523 which caused pjsua_acc_modify() to always reregister regardless of whether there is change or not
git-svn-id: http://svn.pjsip.org/repos/pjproject/trunk@4579 74dad513-b988-da41-8d7b-12977e46ad98

pjsip/src/pjsua-lib/pjsua_acc.c
@@ -1048,6 +1048,7 @@ PJ_DEF(pj_status_t) pjsua_acc_modify( pjsua_acc_id acc_id,
     /* Credential info */
     {
 	unsigned i;
+	pj_bool_t cred_changed = PJ_FALSE;
 
 	/* Selective update credential info. */
 	for (i = 0; i < cfg->cred_count; ++i) {
@@ -1071,6 +1072,8 @@ PJ_DEF(pj_status_t) pjsua_acc_modify( pjsua_acc_id acc_id,
 
 	    /* Not found, insert this */
 	    if (j == acc->cfg.cred_count) {
+		cred_changed = PJ_TRUE;
+
 		/* If account credential is full, discard the last one. */
 		if (acc->cfg.cred_count == PJ_ARRAY_SIZE(acc->cfg.cred_info)) {
     		    pj_array_erase(acc->cfg.cred_info, sizeof(pjsip_cred_info),
@@ -1097,8 +1100,10 @@ PJ_DEF(pj_status_t) pjsua_acc_modify( pjsua_acc_id acc_id,
 	    acc->cred[acc->cred_cnt++] = pjsua_var.ua_cfg.cred_info[i];
 	}
 
-	update_reg = PJ_TRUE;
-	unreg_first = PJ_TRUE;
+	if (cred_changed) {
+	    update_reg = PJ_TRUE;
+	    unreg_first = PJ_TRUE;
+	}
     }
 
     /* Authentication preference */